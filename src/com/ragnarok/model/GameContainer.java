3c81f431e8099448103bdc8959df10335a772c50
Sergio225 <s3rg10.martinez.roman@gmail.com>
2019-03-20 00:34:20 +0100

    A lot of features in progress...

--- src/com/ragnarok/model/GameContainer.java
+++ src/com/ragnarok/model/GameContainer.java
@@ -1,7 +1,42 @@
 package com.ragnarok.model;
 
-public class GameContainer {
+import java.awt.Graphics;
 
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public abstract class GameContainer extends JPanel{
+
+	public GameContainer() {}
 	
+	public GameContainer(int windowWidth, int windowHeight) {
+		
+		this.setWindowWidth(windowWidth);
+		this.setWindowHeigth(windowHeight);
+		
+	}
+
+	public int getWindowWidth() 
+	{
+		return this.getWidth();
+	}
+	
+	public int getWindowHeight()
+	{
+		return this.getHeight();
+	}
+	
+	public void setWindowWidth(int width)
+	{
+		this.setSize(width, this.getWindowHeight());
+	}
+	
+	public void setWindowHeigth(int heigth)
+	{
+		this.setSize(this.getWindowWidth(), heigth);
+	}
+	
+	@Override
+	public abstract void paintComponent(Graphics g);
 	
 }
---
+++ src/com/ragnarok/model/Racket.java
@@ -0,0 +1,12 @@
+package com.ragnarok.model;
+
+import com.ragnarok.controller.utils.Vector2;
+
+public class Racket {
+
+	private Vector2 position;
+	private int size;
+	private Vector2 direction;
+	private Vector2 speed;
+	
+}
